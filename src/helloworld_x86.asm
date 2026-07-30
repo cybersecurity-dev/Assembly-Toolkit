34d6b931cf1c51b2feeae125162aeb7ac0b27d0c
cybersecurity-dev <174958202+cybersecurity-dev@users.noreply.github.com>
2026-07-30 13:58:25 +0200

    Create helloworld_x86.asm
    x86 version added

---
+++ src/helloworld_x86.asm
@@ -0,0 +1,18 @@
+global _start
+
+section .text
+_start:
+    ; write(1, message, 16)
+    mov     eax, 4          ; sys_write
+    mov     ebx, 1          ; stdout
+    mov     ecx, message    ; buffer
+    mov     edx, 16         ; length
+    int     0x80
+
+    ; exit(0)
+    mov     eax, 1          ; sys_exit
+    xor     ebx, ebx        ; status = 0
+    int     0x80
+
+section .data
+message db "Hello, World!..", 10
